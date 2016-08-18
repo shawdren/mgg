4f3fc75f97e57786dc41ccc62b65789199ba429f
Shawdren <jl_lsj@163.com>
2016-08-19 00:05:11 +0800

    update logs

--- application/controllers/admin/logs.php
+++ application/controllers/admin/logs.php
@@ -1,16 +1,35 @@
 <?php
 defined('BASEPATH') OR exit('No direct script access allowed');
 
-class Logs extends Public_Controller {
+class Logs extends Admin_Controller {
 
     public function __construct()
     {
         parent::__construct();
+        /* Title Page :: Common */
+        $this->page_title->push(lang('menu_logs'));
+        $this->data['pagetitle'] = $this->page_title->show();
+
+        /* Breadcrumbs :: Common */
+        $this->breadcrumbs->unshift(1, lang('menu_logs'), 'admin/logs');
     }
 
 
 	public function index()
 	{
-		$this->load->view('public/home', $this->data);
-	}
+        if ( ! $this->ion_auth->logged_in() OR ! $this->ion_auth->is_admin())
+        {
+            redirect('auth/login', 'refresh');
+        }
+        else
+        {
+            /* Breadcrumbs */
+            $this->data['breadcrumb'] = $this->breadcrumbs->show();
+
+            // $this->data['groups'] = $this->ion_auth->groups()->result();
+
+            /* Load Template */
+            $this->template->admin_render('admin/logs/index', $this->data);
+        }
+    }
 }
